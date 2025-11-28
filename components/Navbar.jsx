236e39c3d32537821d2c93c429c1a5f5bd04f432
rolinkstone <rollycyber@gmail.com>
2025-11-28 14:47:02 +0700

    Perubahan Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -62,7 +62,7 @@ export default function Navbar() {
               onClick={() => signIn("keycloak")}
               className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
             >
-              Login
+              Login SSO
             </button>
           )}
         </div>
